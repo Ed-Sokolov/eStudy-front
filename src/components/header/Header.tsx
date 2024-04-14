5c7330f6ac8b65b29f956da134be6b786bcfc091
Ed-Sokolov <ed.sokolov33@gmail.com>
2024-04-14 13:38:47 +0300

    Added styles for header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./header.scss";
 import {ReactComponent as Logo} from "../../assets/img/logo.svg";
+import {ReactComponent as Divider} from "../../assets/img/divider.svg";
 import {NavLink} from "react-router-dom";
 
 export const Header: React.FC = () => {
@@ -15,21 +16,35 @@ export const Header: React.FC = () => {
                     <nav className="nav">
                         <ul className="menu">
                             <li className="menu__item">
-                                <NavLink to={'/'}>
+                                <NavLink to={'/'} className={'link'}>
                                     Main
                                 </NavLink>
                             </li>
                             <li className="menu__item">
-                                <NavLink to={'/rooms'}>
+                                <NavLink to={'/rooms'} className={'link'}>
                                     Rooms
                                 </NavLink>
                             </li>
                         </ul>
                     </nav>
 
-                    <div className="header__user">
-                        Sokolov E. O.
-                    </div>
+                    <ul className="header__log">
+                        <li className="header__log-item">
+                            <NavLink to={'/log-in'} className={'link'}>Log In</NavLink>
+                        </li>
+                        <li className="header__log-item">
+                            <div className="vertical-divider">
+                                <Divider />
+                            </div>
+                        </li>
+                        <li className="header__log-item">
+                            <NavLink to={'/log-up'} className={'link'}>Log Up</NavLink>
+                        </li>
+                    </ul>
+
+                    {/*<div className="header__user">*/}
+                    {/*    Sokolov E. O.*/}
+                    {/*</div>*/}
                 </div>
             </div>
         </header>
