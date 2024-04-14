82e42fe1bdba669907282d1edce1be1b3a4e7349
Ed-Sokolov <ed.sokolov33@gmail.com>
2024-04-14 16:43:36 +0300

    Added styles for log pages

--- src/components/log/log-in/LogIn.tsx
+++ src/components/log/log-in/LogIn.tsx
@@ -3,6 +3,8 @@ import "./../log.scss";
 import { type TLogForm } from "../../../type/log/LogForm";
 import { type TLogInForm } from "../../../type/log/LogInForm";
 import {Field, Form, Formik} from "formik";
+import LogImg from "./../../../assets/img/log-img-1.jpg";
+import LogBackImg from "./../../../assets/img/log-img-2.png";
 
 export const LogIn: React.FC<TLogForm<TLogInForm>> = (
     {
@@ -12,33 +14,48 @@ export const LogIn: React.FC<TLogForm<TLogInForm>> = (
 ) => {
     return (
         <div className="container">
-            <div className="log">
-                <h2>Welcome <span>Back</span></h2>
+            <div className="log log-in">
+                <div className="log__form-wrapper">
+                    <div className="log__content">
+                        <h2 className="log__title">Welcome Back to<br/> Our <span>Sys</span>tem</h2>
 
-                <Formik initialValues={initValues} onSubmit={submit}>
-                    {
-                        ({errors, touched, isValid}) => <Form>
-                            <div className="form-group">
-                                <label htmlFor="email">Email</label>
-                                <Field type={'email'} as={'input'} name={'email'} id={'email'} placeholder={'Your Email'}/>
-                            </div>
+                        <Formik initialValues={initValues} onSubmit={submit}>
+                            {
+                                ({errors, touched, isValid}) => <Form className="log-form">
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <Field type={'email'} as={'input'} name={'email'} id={'email'} placeholder={'Your Email'} className="input"/>
+                                    </div>
 
-                            <div className="form-group">
-                                <label htmlFor="password">Password</label>
-                                <Field type={'password'} as={'input'} name={'password'} id={'password'} placeholder={'Your Password'}/>
-                            </div>
+                                    <div className="form-group">
+                                        <label htmlFor="password">Password</label>
+                                        <Field type={'password'} as={'input'} name={'password'} id={'password'} placeholder={'Your Password'} className="input"/>
+                                    </div>
 
-                            <div className="checkbox-group">
-                                <label htmlFor="checkbox">Remember Me</label>
-                                <Field type={'checkbox'} name={'checkbox'} id={'checkbox'}/>
-                            </div>
+                                    <div className="checkbox-group">
+                                        <label htmlFor="checkbox">Remember Me</label>
+                                        <Field type={'checkbox'} name={'checkbox'} id={'checkbox'}/>
+                                    </div>
 
-                            <div className="form-group">
-                                <button type={'submit'}>Log In</button>
-                            </div>
-                        </Form>
-                    }
-                </Formik>
+                                    <div className="form-group">
+                                        <button type={'submit'} className="button">Log In</button>
+                                    </div>
+                                </Form>
+                            }
+                        </Formik>
+
+                        <div className="divider"></div>
+                    </div>
+
+                    <img src={LogBackImg} alt="" className="log__form-img"/>
+                </div>
+                <div className="log__back">
+                    <img src={LogImg} alt=""/>
+
+                    <div className="author">
+                        Made by <span>Eduard Sokolov</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
